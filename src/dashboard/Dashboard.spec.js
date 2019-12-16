124575ae7d18c98f9eacd02e7f0980638c6a7c4e
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-16 17:25:26 -0600

    added tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+test('App renders correctly', () => {
+  render(<Dashboard />);
+});
+
+describe('<Dashboard />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+test('App renders correctly', () => {
+  render(<Display />);
+});
+
+describe('<Display />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  })
+});
